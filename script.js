b1f1abc315d8777e346f43e9a5c0daa5a1a80408
anwespanda <mail@anwespanda.com>
2023-03-09 03:37:07 +0530

    Added all 5 popup boxes. Added functionality to all shapes. Removed shape animations when hovering, and when popup is active. Added a scaling animation when the page is loaded initially. Added a transition to the blur filter. Changed from popup-container in CSS to .popup.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const wrapper = document.getElementById("wrapper");
 
 const rand = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
-const popup = document.getElementById('popup-container');
+const shapes = document.querySelectorAll(".shape");
 
 const uniqueRand = (min, max, prev) => {
   let next = prev;
@@ -11,7 +11,25 @@ const uniqueRand = (min, max, prev) => {
   
   return next;
 }
+window.onload = () => {
+  const index = uniqueRand(0, combinations.length - 1, prev),
+  combination = combinations[index];
 
+  wrapper.dataset.configuration = combination.configuration;
+  wrapper.dataset.roundness = combination.roundness;
+
+  document.querySelectorAll(".shape").forEach((shape, index) => {
+    shape.animate([
+      { transform: "scale(0)" },
+      { transform: "scale(1)" }
+    ], {
+      duration: 600,
+      easing: "ease-in-out",
+      fill: "forwards",
+      delay: index - 100
+    });
+  });
+};
 const combinations = [
   { configuration: 1, roundness: 1 },
   { configuration: 1, roundness: 2 },
@@ -24,6 +42,8 @@ const combinations = [
 let prev = 0;
 
 setInterval(() => {
+  if (wrapper.matches(':hover')) return;
+  if (wrapper.classList.contains("active")) return;
   const index = uniqueRand(0, combinations.length - 1, prev),
         combination = combinations[index];
   
@@ -33,7 +53,10 @@ setInterval(() => {
   prev = index;
 }, 3000);
 
-function toggle(){
+function toggle(modalId) {
+  const popup = document.getElementById(modalId);
   wrapper.classList.toggle('active');
   popup.classList.toggle('active');
 }
+
+
